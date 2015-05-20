bd065ec82993494cc424b2a3f45f30397b8dd6a5
kwokster10 <kwokster10@hotmail.com>
2015-05-20 11:25:00 -0400

    changed readme

--- README.md
+++ README.md
@@ -4,29 +4,29 @@
 
 
 ### Lesson Plan
-	- Install Python
-		- check via terminal 'python -V'
-		- else install with 'brew install python'
-		- 'ctrl + d' to exit
-	- Introduction to Python
-		- What it is?
-		- What it's used for?
-		- Why do companies and other programmers choose it?
-		- Why you should learn it?
-	- Mini Monty Python Ruby exercise (similar to Bond exercise)
-	- Compare Ruby syntax to Python
-	- Delve more into Python syntax
-		- Conditionals
-		- Looping
-		- Objects 
-		- Extensions (like regex)
-		- Etc.
-	- Python only exercise
+
+- Install Python
+	- check via terminal 'python -V'
+	- else install with 'brew install python'
+	- 'ctrl + d' to exit
+- Introduction to Python
+	- What it is?
+	- What it's used for?
+	- Why do companies and other programmers choose it?
+	- Why you should learn it?
+- Mini Monty Python Ruby exercise (similar to Bond exercise)
+- Delve more into Python syntax
+	- Conditionals
+	- Looping
+	- Objects 
+	- Extensions (like regex)
+	- Etc.
+- Python only exercise
 
 ### Further Resources:
 1. [Google Python Tutorial](https://developers.google.com/edu/python/)
 2. [Main Python Site](http://www.python.org)
-3. [Python Docs]((http://docs.python.org/library)
+3. [Python Docs](http://docs.python.org/library)
 4. [Play More with Python Regex](http://pythex.org/)
 5. [Beginners Breakdown on Github](https://github.com/GA-Tutorials/Python)
 	
--- command_line.py
+++ command_line.py
@@ -1,4 +1,4 @@
-# Create a command line app that will take a string and returns it printed in all caps and in pig latin if the word starts with the letter 'p'. If the word is "Crawford", print "Hey Hey Hey". Otherwise print it in reverse one letter at a time. Hint, Python's equivalent of process.argv[2] is sys.argv[1].
+# Create a command line app that will take a string and returns it printed in pig latin if the word starts with the letter 'p'. If the word is "Crawford", print "Hey Hey Hey". Otherwise print it in reverse one letter at a time in all caps. Hint, Python's equivalent of process.argv[2] is sys.argv[1].
 
 import sys
 import re
@@ -6,16 +6,20 @@ import re
 word = sys.argv[1]
 # checking to see if the word starts with a p followed by a vowel
 vowels = re.match('(p[aeiouy].)', word, flags=re.I)
-# checking to see if the word starts with a p followed by consonant
+# checking to see if the word starts with a p followed by a consonant
 consonant = re.match('(p[^aeiouy].)', word, flags=re.I)
 
-# condition checks for p first, then crawford then others
-if vowels:
+# condition checks for crawford, then p words, then others
+if word.lower() == "crawford":
+	print "Hey " * 3
+elif vowels:
 	print word[1:] + word[0:1] + 'ay'
 elif consonant:
-	print word[2:] + word[:2] + 'ay'
-elif word.lower() == "crawford":
-	print "Hey " * 3
+	print "%s%say" % (word[2:], word[:2])
 else:
 	for letter in word[::-1]:
-		print letter
+		print letter.upper()
+
+# The last else is an extended slice syntax. It works by doing [begin:end:step] - by leaving begin and end off and specifying a step of -1, it reverses a string.
+# Check out https://docs.python.org/2/whatsnew/2.3.html#extended-slices to read more
+
